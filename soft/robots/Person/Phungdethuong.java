6e5887d3724f8cd98a2b3bb34f4b34fd03ce10fd
phungdethuong <44720733+phungdethuong@users.noreply.github.com>
2018-11-10 14:26:07 +0700

    create robot

--- soft/robots/Person/Phungdethuong.java
+++ soft/robots/Person/Phungdethuong.java
@@ -1,73 +1,83 @@
 package Person;
-//import robocode.*;
-//import java.awt.Color.*;
 import robocode.HitRobotEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 import java.awt.*;
 
+import robocode.HitRobotEvent;
 
-// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+public class Phungdethuong extends Robot {
 
-// Resend robot because of bugs
+	boolean seephungdethuong;
+	double moveAmount;
+	double fire;
+	int turnDirection = 1;
 
-/**
- * Phungdethuong - a robot by (your name here)
- */
-public class Phungdethuong extends Robot
-		{
 	/**
-	 * run: Phungdethuong's default behavior
+	 * run: Move around the walls
 	 */
 	public void run() {
-	setBodyColor(Color.pink);
-		setGunColor(Color.green);
-		setRadarColor(Color.pink);
-		setBulletColor(Color.cyan);
-		setScanColor(Color.black);
-		
-	while (true) {
-			turnGunRight(360);
-			ahead(50);
+		// Set colors
+		setBodyColor(Color.pink);
+		setGunColor(Color.blue);
+		setRadarColor(Color.black);
+		setBulletColor(Color.white);
+		setScanColor(Color.red);
+
+		// Initialize moveAmount to the maximum possible for this battlefield.
+		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+		// Initialize peek to false
+		seephungdethuong = false;
+
+		// turnLeft to face a wall.
+		// getHeading() % 90 means the remainder of
+		// getHeading() divided by 90.
+		//turnLeft(getHeading() % 90);
+		//ahead(moveAmount);
+		// Turn the gun to turn right 90 degrees.
+		//seephungdethuong = true;
+		//turnGunRight(90);
+		//turnRight(90);
+
+		while (true) {
+			// Look before we turn when ahead() completes.
+			seephungdethuong = true;
+			// Move up the wall
+			ahead(moveAmount);
+			// Don't look now
+			seephungdethuong = false;
+			// Turn to the next wall
+			//turnRight(90);
+		//}
+	//	while (true) {
+			turnRight(45);
+			ahead(200);
+			turnLeft(45);
+			ahead(200);
+			turnRight(45);
+			back(200);
+			turnLeft(45);
+			back(200);
 			scan();
-		//	turnGunRight(e.getBearing());
 			
-			//ahead(50);
-			//back(50);
 		}
 	}
 
 	/**
-	 * onScannedRobot:  We have a target.  Go get it.
+	 * onHitRobot:  Move away a bit.
 	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-
-	//	if (e.getBearing() >= 0) {
-		//	ahead(50);
-	//	} else {
-		//	back(50);
-		//}
-
-		turnRight(e.getBearing());
-		ahead(e.getDistance() + 50);
-		scan(); // Might want to move ahead again!
-	}
-
-	/**
-	 * onHitRobot:  Turn to face robot, fire hard, and ram him again!
-	 */
-	public void onHitRobot(HitRobotEvent e) {
-	//	if (e.getBearing() >= 0) {
-		//	ahead(10);
-		//} else {
-		//	back(10);
-		//}
+public void onHitRobot(HitRobotEvent e) {
+		if (e.getBearing() >= 0) {
+			turnDirection = 1;
+		} else {
+			turnDirection = -1;
+		}
 		turnRight(e.getBearing());
 
 		// Determine a shot that won't kill the robot...
 		// We want to ram him instead for bonus points
 		if (e.getEnergy() > 16) {
-			fire(10);
+			fire(3);
 		} else if (e.getEnergy() > 10) {
 			fire(2);
 		} else if (e.getEnergy() > 4) {
@@ -77,6 +87,23 @@ public class Phungdethuong extends Robot
 		} else if (e.getEnergy() > .4) {
 			fire(.1);
 		}
-		ahead(40); // Ram him again!
-	}
+		ahead(40);
+
+	/**
+	 * onScannedRobot:  Fire!
+	 */
+public void onScannedRobot(ScannedRobotEvent e) {
+		
+		if (e.getBearing() >= 0) {
+			turnDirection = 1;
+		} else if{
+			turnDirection = -1;
+		}
+
+		turnRight(e.getBearing());
+		ahead(e.getDistance() + 5);
+		scan();
+	}	
+	
+
 }
