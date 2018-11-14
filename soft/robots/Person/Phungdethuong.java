c7a9c836e2a61ed69a078e6b42776e8bb1f1c600
phungdethuong <44720733+phungdethuong@users.noreply.github.com>
2018-11-14 13:08:30 +0700

    Create robot

--- soft/robots/Person/Phungdethuong.java
+++ soft/robots/Person/Phungdethuong.java
@@ -1,64 +1,75 @@
+/**
+ * Copyright (c) 2001-2018 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://robocode.sourceforge.net/license/epl-v10.html
+ */
 package Person;
+
+
 import robocode.HitRobotEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
 import java.awt.*;
 
-import robocode.HitRobotEvent;
 
+/**
+ * Walls - a sample robot by Mathew Nelson, and maintained by Flemming N. Larsen
+ * <p>
+ * Moves around the outer edge with the gun facing in.
+ *
+ * @author Mathew A. Nelson (original)
+ * @author Flemming N. Larsen (contributor)
+ */
 public class Phungdethuong extends Robot {
 
-	boolean seephungdethuong;
-	double moveAmount;
-	double fire;
-	int turnDirection = 1;
+	boolean peek; // Don't turn if there's a robot there
+	double moveAmount; // How much to move
 
 	/**
 	 * run: Move around the walls
 	 */
 	public void run() {
 		// Set colors
-		setBodyColor(Color.pink);
+		setBodyColor(Color.blue);
 		setGunColor(Color.blue);
-		setRadarColor(Color.black);
-		setBulletColor(Color.white);
-		setScanColor(Color.red);
+		setRadarColor(Color.pink);
+		setBulletColor(Color.pink);
+		setScanColor(Color.pink);
 
 		// Initialize moveAmount to the maximum possible for this battlefield.
 		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
 		// Initialize peek to false
-		seephungdethuong = false;
-
+		peek = false;
 		// turnLeft to face a wall.
 		// getHeading() % 90 means the remainder of
 		// getHeading() divided by 90.
-		//turnLeft(getHeading() % 90);
-		//ahead(moveAmount);
+		///turnLeft(getHeading() % 90);
+		turnRight(getHeading() % 90);
+		///ahead(moveAmount);
+		back(moveAmount);
 		// Turn the gun to turn right 90 degrees.
-		//seephungdethuong = true;
-		//turnGunRight(90);
-		//turnRight(90);
+		peek = true;
+		///turnGunRight(90);
+		///turnRight(90);
+		turnGunLeft(90);
+		turnLeft(90);
 
 		while (true) {
 			// Look before we turn when ahead() completes.
-			seephungdethuong = true;
+			peek = true;
+			///peek = false;
 			// Move up the wall
-			ahead(moveAmount);
+			///ahead(moveAmount);
+			back(moveAmount);
 			// Don't look now
-			seephungdethuong = false;
+			peek = false;
+			///peek = true;
 			// Turn to the next wall
-			//turnRight(90);
-		//}
-	//	while (true) {
-			turnRight(45);
-			ahead(200);
-			turnLeft(45);
-			ahead(200);
-			turnRight(45);
-			back(200);
-			turnLeft(45);
-			back(200);
-			scan();
+			///turnRight(90);
+			turnLeft(90);
 			
 		}
 	}
@@ -66,44 +77,34 @@ public class Phungdethuong extends Robot {
 	/**
 	 * onHitRobot:  Move away a bit.
 	 */
-public void onHitRobot(HitRobotEvent e) {
-		if (e.getBearing() >= 0) {
-			turnDirection = 1;
-		} else {
-			turnDirection = -1;
+	public void onHitRobot(HitRobotEvent e) {
+		// If he's in front of us, set back up a bit.
+		if (e.getBearing() > -90 && e.getBearing() < 90) {
+			///back(100);
+			ahead(100);
+		} // else he's in back of us, so set ahead a bit.
+		else {
+			///ahead(100);
+			back(100);
 		}
-		turnRight(e.getBearing());
+		double turnGunAmt = normalRelativeAngleDegrees(e.getBearing() + getHeading() - getGunHeading());
 
-		// Determine a shot that won't kill the robot...
-		// We want to ram him instead for bonus points
-		if (e.getEnergy() > 16) {
-			fire(3);
-		} else if (e.getEnergy() > 10) {
-			fire(2);
-		} else if (e.getEnergy() > 4) {
-			fire(1);
-		} else if (e.getEnergy() > 2) {
-			fire(.5);
-		} else if (e.getEnergy() > .4) {
-			fire(.1);
-		}
-		ahead(40);
+		turnGunRight(turnGunAmt);
+		fire(3);
+	
+
+	}
 
 	/**
 	 * onScannedRobot:  Fire!
 	 */
-public void onScannedRobot(ScannedRobotEvent e) {
-		
-		if (e.getBearing() >= 0) {
-			turnDirection = 1;
-		} else if{
-			turnDirection = -1;
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(5);
+		// Note that scan is called automatically when the robot is moving.
+		// By calling it manually here, we make sure we generate another scan event if there's a robot on the next
+		// wall, so that we do not start moving up it until it's gone.
+		if (peek) {
+			scan();
 		}
-
-		turnRight(e.getBearing());
-		ahead(e.getDistance() + 5);
-		scan();
-	}	
-	
-
+	}
 }
